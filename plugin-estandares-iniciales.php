297f8065b1054f3f1f91c41ba5988c1138c7f656
Kenny Poncio <kenpoc4@gmail.com>
2020-05-20 21:46:55 -0600

    Corrección de pequeños errores, ya 100% funcional

--- plugin-estandares-iniciales.php
+++ plugin-estandares-iniciales.php
@@ -10,7 +10,7 @@ if(!defined('ABSPATH'))die();
 function k_estandares_plugin(){
     $css_url    =   plugin_dir_url( __FILE__ );
 
-    wp_enqueue_style('normalize'    , $css_url . '/assets/css/normalizer.css'   , array(), '8.0.1');
+    wp_enqueue_style('normalize'    , $css_url . '/assets/css/normalize.css'   , array(), '8.0.1');
     wp_enqueue_style('k_style'      , $css_url . '/assets/css/k_style.css'      , array(), '1.0.0');
 
 }
